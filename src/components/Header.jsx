ac491d64c9dcdafe939355a8ffd6d9953b6b10d7
Bermet <oceaninapuddle@gmail.com>
2023-02-22 23:48:40 +0600

    created header style for mobile version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -83,7 +83,7 @@ const Header = () => {
                 initial={{ opacity: 0, scale: 0.6 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.6 }}
-                className="w-40 bg- shadow-xl rounded-lg flex flex-col absolute
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute
         top-12 right-0"
               >
                 {user && user.email === "oceaninapuddle@gmail.com" && (
@@ -104,7 +104,7 @@ const Header = () => {
         </div>
       </div>
       {/* mobile */}
-      <div className="flex md:hidden w-full h-full">
+      <div className="flex items-center justify-between md:hidden w-full h-full">
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} alt="Logo" className="w-11 object-cover" />
           <p className="text-headingColor text-xl font-bold">City</p>
@@ -122,7 +122,7 @@ const Header = () => {
               initial={{ opacity: 0, scale: 0.6 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.6 }}
-              className="w-40 bg- shadow-xl rounded-lg flex flex-col absolute
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute
         top-12 right-0"
             >
               {user && user.email === "oceaninapuddle@gmail.com" && (
@@ -133,7 +133,22 @@ const Header = () => {
                   </p>
                 </Link>
               )}
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+              <ul className="flex flex-col">
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all duration-100 ease-in-out hover:bg-slate-100  px-4 py-2 ">
+                  Home
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all duration-100 ease-in-out hover:bg-slate-100 px-4 py-2 ">
+                  Menu
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all duration-100 ease-in-out hover:bg-slate-100 px-4 py-2 ">
+                  About Us
+                </li>
+                <li className="text-base text-textColor cursor-pointer hover:text-headingColor transition-all duration-100 ease-in-out hover:bg-slate-100 px-4 py-2 ">
+                  Service
+                </li>
+              </ul>
+              <p
+               className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base">
                 Logout
                 <MdLogout />
               </p>
